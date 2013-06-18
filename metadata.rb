1bfe2a6fdc8d97c0de3e37b600e5c2e9a966352d
John Paul Deragon <jpderagon@gmail.com>
2013-06-18 11:46:44 -0700

    blah

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'johnder-myface'
 maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'foo@bar.com'
 license          'All rights reserved'
 description      'Installs/Configures johnder-myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
